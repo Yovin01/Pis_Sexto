4066022f4050a950374383418b785b572b2f2d5a
Hilary_Madelein <madeleycc15@gmail.com>
2023-12-27 17:03:11 -0500

    Estructura Header Login

--- src/fragments/Login.jsx
+++ src/fragments/Login.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import '../css/hilarystyle.css';
+import 'boxicons'
+
 
 
 const Login = () => {
      
-    return {
+    return (
+            
+        <header className='header'>
+            <nav className='navbar'>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Servicios</a>
+                <a href="#">Contactos</a>
+            </nav>
+
+            <form action="#" className="search-bar">
+                <input type="text" placeholder='Buscar...'/>
+                <button type='submit'><i class='bx bx-search-alt-2'></i></button>
+            </form>
+        </header>
 
 
 
-    };
+    );
 };
 
  
