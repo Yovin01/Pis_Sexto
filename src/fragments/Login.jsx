9497076c30ecf8d7cf47cb9c2ab58e0a8f445789
Hilary_Madelein <madeleycc15@gmail.com>
2024-01-16 22:43:03 -0500

    Login v4.0

--- src/fragments/Login.jsx
+++ src/fragments/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/hilarystyle.css';
 import 'boxicons';
+import unlIcon from '../UNL.png';
 
 const Login = () => {
     return (
@@ -15,7 +16,7 @@ const Login = () => {
                     <input type="text" placeholder='Buscar...' />
                     <button type='submit'><i className='bx bx-search-alt-2'></i></button>
                 </form> */}
-                
+
             </header>
 
             <div className='background'>
@@ -27,16 +28,21 @@ const Login = () => {
                     <h2 className='logo'><i className='bx bxs-sun'></i>API RADIACIÓN ULTRAVIOLETA</h2>
                     <div className='text-sci'>
                         <h2>Bienvenido! <br /> <span>A un enfoque más consciente de la exposición solar en Loja</span></h2>
-                        <p> Nuestra plataforma ha sido diseñada pensando en personas que buscan acceder 
-                            a información valiosa sobre los niveles de radiación UV en fechas pasadas. Con nuestra API, ahora es 
+                        <p> Nuestra plataforma ha sido diseñada pensando en personas que buscan acceder
+                            a información valiosa sobre los niveles de radiación UV en fechas pasadas. Con nuestra API, ahora es
                             posible explorar y utilizar datos detallados de radiación UV históricos, esta proporcionará acceso a una fuente confiable y completa de información. </p>
-                       
+
                     </div>
                 </div>
 
                 <div className='logreg-box'>
                     <div className='form-box login'>
+
                         <form action="#">
+                            <div className='iconunl'>
+                                <img src={unlIcon} alt="Logo UNL" />
+
+                            </div>
                             <h2>
                                 Iniciar Sesión
                             </h2>
