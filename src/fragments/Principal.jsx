81495adf63aeb251fbfc27b95c6f9ab5ae6266ec
Yovin01 <Yovin01@Yovin01>
2024-02-19 12:17:53 -0500

    areglo

--- src/fragments/Principal.jsx
+++ src/fragments/Principal.jsx
@@ -7,6 +7,9 @@ import { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import Grafica from './Grafica';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import customMarkerImage from '../img/puntoUbi/map-pin3.png';
 import ChatBot from './ChatBot';
 import { getUVD, getUVP } from '../utiles/ides';
@@ -113,6 +116,14 @@ const Principal = () => {
     cargarMapa();
     cargarDatos();
   }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  
 
   const getColorByUVValue = (uvValue) => {
     if (uvValue >= 0 && uvValue <= 3) {
@@ -127,6 +138,72 @@ const Principal = () => {
       return 'bg-danger'; // Morado para valores mayores a 11
     }
   };
+  // Función para determinar las recomendaciones basadas en la medida (getUVP)
+const cards = () => {
+  const measure = getUVP(); // Obtener el valor de medida adecuado, puedes ajustarlo según tu lógica
+
+  if (measure >= 0.0 && measure < 3.0) {
+    return (
+      <div>
+        <h5>Necesita protección mínima</h5>
+        <p>Riesgo: Bajo</p>
+        <h5>Use gafas con filtro UV</h5>
+        <p>Riesgo: Bajo</p>
+      </div>
+    );
+  } else if (measure >= 3.0 && measure < 6.0) {
+    return (
+      <div>
+        <h5>Use gorra o sombrero</h5>
+        <p>Riesgo: Moderado</p>
+        <h5>Use gafas con filtro UV</h5>
+        <p>Riesgo: Moderado</p>
+        <h5>Utilice crema con filtro solar</h5>
+        <p>Riesgo: Moderado</p>
+      </div>
+    );
+  } else if (measure >= 6.0 && measure < 8.0) {
+    return (
+      <div>
+        <h5>Use gorra o sombrero</h5>
+        <p>Riesgo: Alto</p>
+        <h5>Use gafas con filtro UV</h5>
+        <p>Riesgo: Alto</p>
+        <h5>Utilice crema con filtro solar</h5>
+        <p>Riesgo: Alto</p>
+      </div>
+    );
+  } else if (measure >= 8.0 && measure < 11.0) {
+    return (
+      <div>
+        <h5>Use gorra o sombrero</h5>
+        <p>Riesgo: Muy Alto</p>
+        <h5>Use gafas con filtro UV</h5>
+        <p>Riesgo: Muy Alto</p>
+        <h5>Utilice crema con filtro solar</h5>
+        <p>Riesgo: Muy Alto</p>
+        <h5>Procure no exponerse al sol</h5>
+        <p>Riesgo: Muy Alto</p>
+      </div>
+    );
+  } else if (measure >= 11.0 && measure <= 15.0) {
+    return (
+      <div>
+        <h5>Evite la exposición al sol</h5>
+        <p>Riesgo: Extremo</p>
+        <h5>Utilice crema con filtro solar alto SPF 30+</h5>
+        <p>Riesgo: Muy Alto</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h5>Lo sentimos presentamos algunos problemas...</h5>
+      </div>
+    );
+  }
+};
+
 
 
   return (
@@ -197,13 +274,34 @@ const Principal = () => {
             </div>
             {/* Histograma de indice UV */}
             <div className="row">
-              <div className="col-xs-12">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
                 <div className="panel panel-default" style={{ height: 350 }}>
-                  <div className="panel-heading text-center">Grafico Histograma</div>
-                  <Grafica />
+                  <div className="panel-heading text-center">Recomendaciones</div>
+                  <div className="card">
+  <div className="card-body">
+    {cards()} {/* Aquí se integran las recomendaciones */}
+  </div>
+</div>
+
                 </div>
 
               </div>
+            
+      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+        <Slider {...settings}>
+          {pDisp.map((dispositivo) => (
+            <div key={dispositivo.dispositivoId}>
+              <div className="panel panel-default" style={{ height: 350 }}>
+                <div className="panel-heading text-center"> {dispositivo.nombre}</div>
+                {/* Pasa el dispositivoId como prop a Grafica */}
+                <Grafica dispositivoId={dispositivo.dispositivoId} />
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+ 
+            
             </div>
           </dir>
           <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
